0949372ef302be78a12058eca83f5663a83b1b87
manugolem <manuelcarreras2013@gmail.com>
2026-02-16 23:42:09 -0300

    add: implement texture loading and configure texture parameters in Shader class

--- src/configShader.cpp
+++ src/configShader.cpp
@@ -1,11 +1,36 @@
 #include "../include/configShader.h"
+#include "../include/stb_image.h"
 #include <iostream>
 using namespace std;
+unsigned int cargarTextura(const char* ruta) {
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    // Configurar parámetros de la textura
+
+    int width, height, nrChannels;
+    // Cargar la imagen (stb_image carga la imagen volteada? lo veremos)
+    stbi_set_flip_vertically_on_load(true); // Los PNG normalmente tienen Y invertida
+    unsigned char* data = stbi_load(ruta, &width, &height, &nrChannels, 0);
+
+    if (data) {
+        // Asumimos que el atlas es RGBA (4 canales)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        cout << "Textura cargada: " << ruta << " (" << width << "x" << height << ", " << nrChannels << " canales)" << endl;
+    } else {
+        cout << "ERROR: No se pudo cargar la textura: " << ruta << endl;
+    }
+
+    stbi_image_free(data); // Liberar memoria de la imagen
+    return textureID;
+}
 Shader::Shader() {
     unsigned int vs = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vs, 1, &vertexShaderSrc, NULL);
     glCompileShader(vs);
-    //LOGS idk
+    // LOGS idk
     int success;
     char infoLog[512];
     glGetShaderiv(vs, GL_COMPILE_STATUS, &success);
@@ -43,13 +68,26 @@ Shader::Shader() {
     viewLoc = glGetUniformLocation(shaderProgram, "view");
     projLoc = glGetUniformLocation(shaderProgram, "projection");
 
+    use();
     // Configurar matriz de proyección (se hace una sola vez)
-    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 640.0f / 480.0f, 0.1f, 100.0f);
+    glm::mat4 projection = glm::perspective(glm::radians(90.0f), 1280.0f / 720.0f, 0.1f, 100.0f);
     setProjectionMatrix(glm::value_ptr(projection));
 
     // Configurar matriz model por defecto
     glm::mat4 model = glm::mat4(1.0f);
     setModelMatrix(glm::value_ptr(model));
+
+    textureID = cargarTextura("../textures/textures.png");
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+                    GL_NEAREST); //
+    glUniform1i(glGetUniformLocation(shaderProgram, "textureBlock"), 0);
+    float textureSize = 1/16.0f;
+    glUniform1f(glGetUniformLocation(shaderProgram, "textureSize"), textureSize);
 }
 Shader::~Shader() {
     glDeleteProgram(shaderProgram);
@@ -58,17 +96,14 @@ void Shader::use() {
     glUseProgram(shaderProgram);
 }
 void Shader::setModelMatrix(const float* matrix) {
-    glUseProgram(shaderProgram);
     glUniformMatrix4fv(modelLoc, 1, GL_FALSE, matrix);
 }
 
 void Shader::setViewMatrix(const float* matrix) {
-    glUseProgram(shaderProgram);
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, matrix);
 }
 
 void Shader::setProjectionMatrix(const float* matrix) {
-    glUseProgram(shaderProgram);
     glUniformMatrix4fv(projLoc, 1, GL_FALSE, matrix);
 }
 
@@ -96,14 +131,30 @@ void ChunkBuffer::uploadData(const std::vector<float>& vertices, const std::vect
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
 
-    // Configurar atributo de posición (location = 0)
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // Posición (3 floats) - location 0
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 10 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // Desenlazar (opcional)
+    // Color (3 floats) - location 1
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 10 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // Coordenadas de textura (2 floats) - location 2
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 10 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    // Offset de textura (2 floats) - location 3
+    glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, 10 * sizeof(float), (void*)(8 * sizeof(float)));
+    glEnableVertexAttribArray(3);
+
+    // Verificación opcional
+    GLenum err = glGetError();
+    if (err != GL_NO_ERROR) {
+        std::cerr << "OpenGL error in uploadData: " << err << std::endl;
+    }
+
     glBindVertexArray(0);
 }
-
 void ChunkBuffer::render() {
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
