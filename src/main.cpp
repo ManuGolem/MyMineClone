ceea76ad11c045746406f27ea266ef56a0c67b72
manugolem <manuelcarreras2013@gmail.com>
2026-02-17 12:51:01 -0300

    add: initialize and render DebugAxes in the main loop

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,8 @@ int main() {
     world.generateFlatWorld(160, 160);
 
     Uint32 lastFrame = SDL_GetTicks();
-
+    DebugAxes debugAxes;
+    debugAxes.inicializar();
     int frameCount = 0;
     float fpsTimer = 0.0f;
     while (screen.isRunning()) {
@@ -36,6 +37,25 @@ int main() {
         glm::mat4 view = screen.getCamera().getViewMatrix();
 
         world.render(screen.getCamera().getPosition(), view);
+
+        // a borrar
+        Chunk::sharedShader->use();
+        Chunk::sharedShader->setUseTexture(false);
+
+        // Matriz modelo identidad (ejes en el origen)
+        glm::mat4 model = glm::mat4(1.0f);
+        Chunk::sharedShader->setModelMatrix(glm::value_ptr(model));
+        Chunk::sharedShader->setViewMatrix(glm::value_ptr(view));
+        // La proyección ya está seteada en el shader
+
+        // Desactivar culling para ver las líneas desde cualquier lado
+        glDisable(GL_CULL_FACE);
+
+        debugAxes.renderizar();
+
+        // Restaurar
+        glEnable(GL_CULL_FACE);
+        Chunk::sharedShader->setUseTexture(true);
 
         screen.swap();
     }
