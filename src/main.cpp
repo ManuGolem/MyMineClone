46a4741b364d2e723238a148f7e5d9721c991405
manugolem <manuelcarreras2013@gmail.com>
2026-02-18 00:52:11 -0300

    add: implement right-click block placement logic in main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include "../include/configShader.h"
 #include "../include/screen.h"
 #include "../include/world.h"
-// vertex data
-
+#include <climits>
 int main() {
     Screen screen;
     World world;
-
     world.generateFlatWorld(160, 160);
 
     Uint32 lastFrame = SDL_GetTicks();
@@ -15,10 +13,12 @@ int main() {
     int frameCount = 0;
     float fpsTimer = 0.0f;
     while (screen.isRunning()) {
+        // Limpiar frame anterior
+        screen.clear();
+        // FPS y deltatime
         Uint32 currentFrame = SDL_GetTicks();
         float deltaTime = (currentFrame - lastFrame) / 1000.0f;
         lastFrame = currentFrame;
-
         frameCount++;
         fpsTimer += deltaTime;
         if (fpsTimer >= 1.0f) {
@@ -28,35 +28,52 @@ int main() {
             frameCount = 0;
             fpsTimer = 0;
         }
+        // Inputs
         screen.poll(deltaTime);
-        screen.clear();
-
+        if (screen.wasRightClicked()) {
+            vec3 pos = screen.getCamera().getPosition();
+            vec3 front = screen.getCamera().getFront();
+            float x = 0.0;
+            bool encontreBloque = false;
+            while (!encontreBloque) {
+                vec3 posibleBloque = pos + front * x;
+                if (world.getBlock(posibleBloque.x, posibleBloque.y, posibleBloque.z).active) {
+                    encontreBloque = true;
+                    ivec2 posChunk = world.getChunkPos(posibleBloque);
+                    Chunk* chunk = world.getChunk(posChunk.x, posChunk.y);
+                    Block block;
+                    block.active = true;
+                    block.type = 2;
+                    int offsetx = chunk->getNroChunkX() * 16;
+                    int offsetz = chunk->getNroChunkZ() * 16;
+                    chunk->setBlock(posibleBloque.x - offsetx, posibleBloque.y + 1, posibleBloque.z - offsetz, block);
+                } else {
+                    x += 1.0f;
+                }
+            }
+            screen.clearRightClick();
+        }
+        // Update logicas
         world.update();
 
         // Matriz de la camara
         glm::mat4 view = screen.getCamera().getViewMatrix();
-
+        // Dibujar mundo
         world.render(screen.getCamera().getPosition(), view);
 
-        // a borrar
+        // Renderiuzar ejes de debug (Provisorio)
         Chunk::sharedShader->use();
         Chunk::sharedShader->setUseTexture(false);
-
-        // Matriz modelo identidad (ejes en el origen)
         glm::mat4 model = glm::mat4(1.0f);
         Chunk::sharedShader->setModelMatrix(glm::value_ptr(model));
         Chunk::sharedShader->setViewMatrix(glm::value_ptr(view));
-        // La proyección ya está seteada en el shader
-
-        // Desactivar culling para ver las líneas desde cualquier lado
         glDisable(GL_CULL_FACE);
-
         debugAxes.renderizar();
-
-        // Restaurar
         glEnable(GL_CULL_FACE);
         Chunk::sharedShader->setUseTexture(true);
-
+        // Renderizar menu
+        screen.renderMenu();
+        // Cambiar los back/front frames
         screen.swap();
     }
     return 0;
