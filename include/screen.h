9d85a39e4c4435a4d29f5040ce2ea479fc90419a
manugolem <manuelcarreras2013@gmail.com>
2026-02-19 19:47:23 -0300

    agrego lo del leftClick

--- include/screen.h
+++ include/screen.h
@@ -21,6 +21,7 @@ class Screen {
     bool openMenu = false;
     bool gameInPause = false;
     bool rightClicked = false;
+    bool leftClicked = false;
     int windowWidth = 1280;
     int windowHeight = 720;
     bool crosshairVisible = true;
@@ -38,12 +39,19 @@ class Screen {
     void poll(float deltaTime);
     void clear();
     void swap();
+    void resize();
     void renderMenu();
     void renderCrosshair();
     void renderDebugAxes(const glm::mat4& view, const glm::mat4& projection);
     void renderBlockOutline(int x, int y, int z);
     bool wasRightClicked() const {
         return rightClicked;
+    }
+    bool wasLeftClicked() const {
+        return leftClicked;
+    }
+    void clearLeftClick() {
+        leftClicked = false;
     }
     void clearRightClick() {
         rightClicked = false;
