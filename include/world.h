5654c46286125ac0fc542edc6de5732a2cc7b411
manugolem <manuelcarreras2013@gmail.com>
2026-02-19 19:47:35 -0300

    matriz de projeccion al render

--- include/world.h
+++ include/world.h
@@ -12,6 +12,6 @@ class World {
     Chunk* getChunk(int chunkX, int chunkZ);
     void generateFlatWorld(int width, int depth);
     Block getBlock(int x, int y, int z);
-    void render(vec3 cameraPos, mat4 view);
+    void render(vec3 cameraPos, mat4 view, mat4 projection);
     void update();
 };
