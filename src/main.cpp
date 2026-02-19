c01760a433e38760a7952467b8c4d20ac61d14b7
manugolem <manuelcarreras2013@gmail.com>
2026-02-19 19:49:38 -0300

    creo el raycast mejorado, ahora sabe de donde viene el rayo, asi se puede colocar el bloque desde donde viene

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,38 @@
 #include "../include/configShader.h"
 #include "../include/screen.h"
 #include "../include/world.h"
-#include <climits>
+bool detectBlock(Screen& screen, World& mundo, vec3& blockSelect, vec3& blockFace, bool& colocar) {
+    vec3 pos = screen.getCamera().getPosition();
+    vec3 front = screen.getCamera().getFront();
+    float x = 0.0;
+    bool encontreBloque = false;
+    while (!encontreBloque && x < 4) {
+        int prevx = round(pos.x + front.x * x);
+        int prevy = ceil(pos.y + front.y * x);
+        int prevz = round(pos.z + front.z * x);
+        x += 0.1f;
+        int px = round(pos.x + front.x * x);
+        int py = ceil(pos.y + front.y * x);
+        int pz = round(pos.z + front.z * x);
+        if (pz < 0 || px < 0) {
+            return false;
+        }
+        vec3 posibleBloque = {px, py, pz};
+        if (mundo.getBlock(posibleBloque.x, posibleBloque.y, posibleBloque.z).active) {
+            blockFace = {prevx, prevy, prevz};
+            blockSelect = posibleBloque;
+            encontreBloque = true;
+            if (!mundo.getBlock(blockFace.x, blockFace.y, blockFace.z).active) {
+                colocar = true;
+            }
+        }
+    }
+    return encontreBloque;
+}
 int main() {
     Screen screen;
     World world;
-    world.generateFlatWorld(16, 16);
+    world.generateFlatWorld(160, 160);
 
     Uint32 lastFrame = SDL_GetTicks();
     int frameCount = 0;
@@ -28,36 +55,47 @@ int main() {
         }
         // Inputs
         screen.poll(deltaTime);
-        if (screen.wasRightClicked()) {
-            vec3 pos = screen.getCamera().getPosition();
-            vec3 front = screen.getCamera().getFront();
-            float x = 0.0;
-            bool encontreBloque = false;
-            while (!encontreBloque) {
-                vec3 posibleBloque = pos + front * x;
-                if (world.getBlock(posibleBloque.x, posibleBloque.y, posibleBloque.z).active) {
-                    encontreBloque = true;
-                    ivec2 posChunk = world.getChunkPos(posibleBloque);
-                    Chunk* chunk = world.getChunk(posChunk.x, posChunk.y);
-                    Block block;
-                    block.active = true;
-                    block.type = 2;
-                    int offsetx = chunk->getNroChunkX() * 16;
-                    int offsetz = chunk->getNroChunkZ() * 16;
-                    chunk->setBlock(posibleBloque.x - offsetx, posibleBloque.y + 1, posibleBloque.z - offsetz, block);
-                } else {
-                    x += 1.0f;
-                }
+        vec3 blockPos;
+        vec3 blockFace;
+        bool colocar = false;
+        if (detectBlock(screen, world, blockPos, blockFace, colocar)) {
+            // Renderizar outline del bloque a mirar
+            screen.renderBlockOutline(blockPos.x, blockPos.y, blockPos.z);
+
+            if (screen.wasRightClicked() && colocar) {
+                ivec2 posChunk = world.getChunkPos(blockFace);
+                Chunk* chunk = world.getChunk(posChunk.x, posChunk.y);
+
+                Block block;
+                block.active = true;
+                block.type = 2;
+
+                int offsetx = chunk->getNroChunkX() * 16;
+                int offsetz = chunk->getNroChunkZ() * 16;
+                chunk->setBlock(blockFace.x - offsetx, blockFace.y, blockFace.z - offsetz, block);
             }
-            screen.clearRightClick();
+            if (screen.wasLeftClicked()) {
+                ivec2 posChunk = world.getChunkPos(blockPos);
+                Chunk* chunk = world.getChunk(posChunk.x, posChunk.y);
+
+                Block block;
+                block.active = false;
+                block.type = 0;
+
+                int offsetx = chunk->getNroChunkX() * 16;
+                int offsetz = chunk->getNroChunkZ() * 16;
+                chunk->setBlock(blockPos.x - offsetx, blockPos.y, blockPos.z - offsetz, block);
+            }
         }
+        screen.clearLeftClick();
+        screen.clearRightClick();
         // Update logicas
         world.update();
 
         // Matriz de la camara
         glm::mat4 view = screen.getCamera().getViewMatrix();
         // Dibujar mundo
-        world.render(screen.getCamera().getPosition(), view);
+        world.render(screen.getCamera().getPosition(), view, screen.getCamera().getProjectionMatrix());
         // Renderizar menu
         screen.renderMenu();
         screen.renderCrosshair();
