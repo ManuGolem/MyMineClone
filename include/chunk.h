6289292a98be89933c59725b824a358e96490900
manugolem <manuelcarreras2013@gmail.com>
2026-02-19 23:41:55 -0300

    creo delete para borrar/crear in game

--- include/chunk.h
+++ include/chunk.h
@@ -1,4 +1,4 @@
-#pragma once    
+#pragma once
 #include "configShader.h"
 #include <cmath>
 #include <glm/ext/vector_int2.hpp>
@@ -7,43 +7,39 @@
 using namespace std;
 using namespace glm;
 struct Block {
-    bool active;
-    int type;
+  bool active;
+  int type;
 };
 struct Rectangulo {
-    int x1, x2, y1, y2;
-    int tipoBloque;
+  int x1, x2, y1, y2;
+  int tipoBloque;
 };
 
 class Chunk {
-  private:
-    Block blocks[16][256][16];
-    vector<float> vertexData;
-    vector<unsigned int> indexData;
-    bool needsUpdate;
-    int nroChunkX;
-    int nroChunkZ;
-    int caras;
-    int vertexCount;
-    void generateMesh();
-    ChunkBuffer chunkBuffer;
-    void cargarVertices(const Rectangulo& r, int eje, int direccion, int fijo, int tipoBloque);
+private:
+  Block blocks[16][256][16];
+  vector<float> vertexData;
+  vector<unsigned int> indexData;
+  bool needsUpdate;
+  int nroChunkX;
+  int nroChunkZ;
+  int caras;
+  int vertexCount;
+  void generateMesh();
+  ChunkBuffer chunkBuffer;
+  void cargarVertices(const Rectangulo &r, int eje, int direccion, int fijo,
+                      int tipoBloque);
 
-  public:
-    static Shader* sharedShader;
-    Chunk();
-    void setBlock(int x, int y, int z, const Block& block);
-    Block getBlock(int x, int y, int z) const;
-    void render();
-    bool isEmpty() const;
-    void setNroChunk(int, int);
-    int getCaras() const {
-        return caras;
-    }
-    int getNroChunkX() const {
-        return nroChunkX;
-    }
-    int getNroChunkZ() const {
-        return nroChunkZ;
-    }
+public:
+  static Shader *sharedShader;
+  Chunk();
+  void setBlock(int x, int y, int z, const Block &block);
+  Block getBlock(int x, int y, int z) const;
+  void render();
+  bool isEmpty() const;
+  void setNroChunk(int, int);
+  int getCaras() const { return caras; }
+  int getNroChunkX() const { return nroChunkX; }
+  int getNroChunkZ() const { return nroChunkZ; }
+  void cleanup();
 };
