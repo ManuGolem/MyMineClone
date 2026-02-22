84e8408e5031a6c4e35570f8be232b00046bba78
manugolem <manuelcarreras2013@gmail.com>
2026-02-22 01:50:33 -0300

    Refactor Camera class formatting and adjust field visibility

--- include/camera.h
+++ include/camera.h
@@ -3,34 +3,36 @@
 #include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 class Camera {
-private:
-  vec3 position;
-  vec3 front;
-  vec3 frontHorizontal;
-  vec3 up;
-  vec3 right;
-  vec3 worldUp;
-  float yaw;
-  float pitch;
+  private:
+    vec3 position;
+    vec3 front;
+    vec3 frontHorizontal;
+    vec3 up;
+    vec3 right;
+    vec3 worldUp;
+    float yaw;
+    float pitch;
 
-  float fov = 90;
-  float aspect;
-  float nearPlane = 0.1f;
-  float farPlane = 500.0f;
+    float fov = 120;
+    float aspect;
+    float nearPlane = 0.1f;
+    float farPlane = 500.0f;
 
-public:
-  Camera();
-  void updateCameraVectors();
-  void moveForward(float);
-  void moveBackward(float);
-  void moveLeft(float);
-  void moveRight(float);
-  void moveUp(float);
-  void moveDown(float);
-  void processMouse(float, float);
-  mat4 getViewMatrix();
-  vec3 getFront() const { return front; }
-  vec3 getPosition();
-  mat4 getProjectionMatrix();
-  void setAspectRatio(float width, float height);
+  public:
+    Camera();
+    void updateCameraVectors();
+    void moveForward(float);
+    void moveBackward(float);
+    void moveLeft(float);
+    void moveRight(float);
+    void moveUp(float);
+    void moveDown(float);
+    void processMouse(float, float);
+    mat4 getViewMatrix();
+    vec3 getFront() const {
+        return front;
+    }
+    vec3 getPosition();
+    mat4 getProjectionMatrix();
+    void setAspectRatio(float width, float height);
 };
