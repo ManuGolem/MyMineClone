a85f4905e9afbc592636a47ed22b38372aa742a0
manugolem <manuelcarreras2013@gmail.com>
2026-02-25 01:36:21 -0300

    Refactor Chunk class: add mutex for thread safety, update needsBufferUpdate logic, and restore generateMesh method

--- include/chunk.h
+++ include/chunk.h
@@ -1,9 +1,12 @@
 #pragma once
 #include "configShader.h"
+#include <atomic>
 #include <cmath>
 #include <glm/ext/vector_int2.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
+#include <memory>
+#include <mutex>
 using namespace std;
 using namespace glm;
 class World;
@@ -22,16 +25,18 @@ class Chunk {
     Block blocks[16][256][16];
     vector<float> vertexData;
     vector<unsigned int> indexData;
-    bool needsUpdate;
     int nroChunkX;
     int nroChunkZ;
     int caras;
     int vertexCount;
-    void generateMesh();
-    ChunkBuffer chunkBuffer;
-    void cargarVertices(const Rectangulo& r, int eje, int direccion, int fijo, int tipoBloque);
+    mutex mutexVertex;
+    atomic<bool> needsBufferUpdate{true};
+    unique_ptr<ChunkBuffer> chunkBuffer;
+    void cargarVertices(const Rectangulo& r, int eje, int direccion, int fijo, int tipoBloque, vector<float>& vData, vector<unsigned int>& iData, unsigned int& vCount, int& carasLocal);
 
   public:
+    mutex mutexUpdate;
+    bool needsUpdate = true;
     static Shader* sharedShader;
     Chunk();
     void setBlock(int x, int y, int z, const Block& block);
@@ -39,6 +44,7 @@ class Chunk {
     void render();
     bool isEmpty() const;
     void setNroChunk(int, int);
+    void generateMesh();
     int getCaras() const {
         return caras;
     }
@@ -53,6 +59,8 @@ class Chunk {
         world = w;
     }
     void setUpdate() {
+        lock_guard<mutex> lock(mutexUpdate);
         needsUpdate = true;
     };
+    void setBlockSafe(int x, int y, int z, const Block& block);
 };
