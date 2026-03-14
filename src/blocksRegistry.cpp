2ed23bd05d99dbe79edca1e0eacd5d03989438a1
manugolem <manuelcarreras2013@gmail.com>
2026-03-14 13:41:13 -0300

    agrego bloques

--- src/blocksRegistry.cpp
+++ src/blocksRegistry.cpp
@@ -1,11 +1,41 @@
 #include "../include/blocksRegistry.h"
+#include <algorithm>
 const unordered_map<string, int> BlockRegistry::blockTypes = {
+
     {"stone", 2},
     {"dirt", 3},
     {"grass_block", 4},
+    {"oak_planks", 5},
+    {"smooth_stone_slab", 6},
+    {"smooth_stone", 7},
+    {"bricks", 8},
+    {"tnt", 9},
+    {"water_bucket", 15},
+    {"oak_sapling", 16},
+    {"cobblestone", 17},
+    {"bedrock", 18},
+    {"oak_log", 21},
+    {"iron_block", 23},
+    {"gold_block", 24},
+    {"diamond_block", 25},
+    {"emerald_block", 26},
+    {"redstone_block", 27},
+    {"cyan_wool", 210},
+    {"bookshelf", 36},
+    {"oak_sign", 257},
+    {"redstone", 258},
+    {"compass", 259},
+    {"diamond_pickaxe", 260},
+    {"netherite_sword", 261},
+    {"golden_apple", 262},
+    {"iron_ingot", 263},
+    {"creeper_spawn_egg", 264},
+    {"chest", 265}
+
 };
 
 const unordered_map<Category, std::vector<int>> BlockRegistry::categories = {
+
     {Category::BuildingBlocks, {8}},
     {Category::ColoredBlocks, {210}},
     {Category::NaturalBlocks, {4}},
@@ -16,8 +46,18 @@ const unordered_map<Category, std::vector<int>> BlockRegistry::categories = {
     {Category::FoodDrinks, {262}},
     {Category::Ingredients, {263}},
     {Category::SpawnEggs, {264}},
-    {Category::All, {2, 3, 4, 5, 6, 7, 8, 17, 36, 210, 257, 258, 259, 260, 261, 262, 263, 264, 265}}};
+    {Category::All,
+     []() {
+         vector<int> all;
+         all.reserve(blockTypes.size());
+         for (const auto& pair : blockTypes) {
+             all.push_back(pair.second);
+         }
+         sort(all.begin(), all.end());
+         return all;
+     }()}
 
+};
 const vector<int>& BlockRegistry::get(Category category) {
     return categories.at(category);
 }
